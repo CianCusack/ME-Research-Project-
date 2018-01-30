6b00fa8551eeabb26330ec1fac1b25b9cc5a09cc
CianCusack <cianncusack@gmail.com>
2018-01-30 12:35:07 +0000

    Refactored the boat detector, added comments

--- Code/boat_detector.py
+++ Code/boat_detector.py
@@ -1,67 +1,49 @@
-# USAGE
-# python deep_learning_object_detection.py --image images/example_01.jpg \
-#	--prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
-
-# import the necessary packages
 import numpy as np
 import cv2
 
 
 def detect_boats(image):
     confidence_thresh = 0.1
-    protext = "../deep learning/MobileNetSSD_deploy.prototxt.txt"
-    model = "../deep learning/MobileNetSSD_deploy.caffemodel"
-    # initialize the list of class labels MobileNet SSD was trained to
-	# detect, then generate a set of bounding box colors for each class
+    # Really only care about boats but can detect others
     classes = ["background", "aeroplane", "bicycle", "bird", "boat",
 		"bottle", "bus", "car", "cat", "chair", "cow", "dining table",
 		"dog", "horse", "motorbike", "person", "potted plant", "sheep",
 		"sofa", "train", "tv monitor"]
 
-    # load our serialized model from disk
-    net = cv2.dnn.readNetFromCaffe(protext, model)
+    # load our serialised model from disk
+    net = cv2.dnn.readNetFromCaffe("../deep learning/MobileNetSSD_deploy.prototxt.txt",
+                                   "../deep learning/MobileNetSSD_deploy.caffemodel")
 
-    # load the input image and construct an input blob for the image
-	# by resizing to a fixed 300x300 pixels and then normalizing it
-	# (note: normalization is done via the authors of the MobileNet SSD
-	# implementation)
+    # current frame is used to construct an input blob by resizing to a
+    # fixed 300x300 pixels and then normalizing it
+    # changing resizing alters accuracy and as a result performance
     (h, w) = image.shape[:2]
     blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 0.007843, (300, 300), 127.5)
 
-    # pass the blob through the network and obtain the detections and
-	# predictions
+    # pass the blob through the network and obtain the detections and predictions
     net.setInput(blob)
     detections = net.forward()
 
-    # loop over the detections
     boats = []
-    coords = []
+    boat_positions = []
     for i in np.arange(0, detections.shape[2]):
-        # extract the confidence (i.e., probability) associated with the
-		# prediction
-
+        #Get the confidence of the object found
         confidence = detections[0, 0, i, 2]
 
-        # filter out weak detections by ensuring the `confidence` is
-		# greater than the minimum confidence
+        # Ignore detections below confidence or errors where background is detected
+        # i.e. confidence should always be between threshold and 1, ignore all else
         if confidence > confidence_thresh and confidence <= 1:
-            # extract the index of the class label from the `detections`,
-		    # then compute the (x, y)-coordinates of the bounding box for
-		    # the object
+            # Compute the position of the boat and add it to the boat_positions array
             idx = int(detections[0, 0, i, 1])
             box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
             startX, startY, endX, endY = box.astype("int")
-            coords.append([startX, startY, endX, endY])
+            boat_positions.append([startX, startY, endX, endY])
             boat = image[startY:endY, startX: endX].copy()
             boats.append(boat)
 
             # display the prediction
             label = "{} {}: {:.2f}%".format(classes[idx], i+1, confidence * 100.0)
             cv2.rectangle(image, (startX, startY), (endX, endY), (0,0,255), 2)
-            y = startY - 15 if startY - 15 > 15 else startY + 15
-            cv2.putText(image, label, (startX, y),
+            cv2.putText(image, label, (startX, startY-3),
 			    cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,0), 2)
-
-    # show the output image
-    #cv2.imshow("Output", image)
-    return boats, coords
+    return boats, boat_positions
